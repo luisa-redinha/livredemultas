c40265410ec4911ed4d9e81a437e8b30a240bbe5
Rafa Lopes <rafalopessecond@gmail.com>
2024-09-19 18:17:59 +0100

    create IconButton

---
+++ components/buttons/IconButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+
+enum ICONS {
+	whatsapp = "fa-brands fa-whatsapp",
+	phone = "fa-solid fa-phone",
+	email = "fa-solid fa-envelope",
+}
+
+export type IconButtonProps = {
+	onClick?: Function;
+	icon: keyof typeof ICONS;
+};
+
+export default function IconButton(props: IconButtonProps) {
+	return (
+		<button
+			className="icon-button"
+			onClick={() => props?.onClick && props.onClick()}
+		>
+			<i className={ICONS[props.icon]} />
+		</button>
+	);
+}
