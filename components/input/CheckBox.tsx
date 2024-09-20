5a25c9a8254010531bcea074b4eb77681646337b
Rafa Lopes <rafalopessecond@gmail.com>
2024-09-20 16:13:05 +0100

    update checkbox

--- components/input/CheckBox.tsx
+++ components/input/CheckBox.tsx
@@ -1,27 +1,33 @@
 "use client";
 
-import { ChangeEventHandler } from "react";
+import { forwardRef, InputHTMLAttributes, Ref } from "react";
 
-type CheckBoxProps = {
-	children: string;
-	onChange?: ChangeEventHandler<HTMLInputElement>;
-	className?: string;
-};
-
-export default function CheckBox(props: CheckBoxProps) {
-	return (
-		<label className={"checkbox " + props.className}>
-			<span className="checkbox__toggle">
-				<input
-					type="checkbox"
-					className="checkbox__input"
-					aria-hidden
-					onChange={props.onChange}
-				/>
-				<span className="checkbox__toggle__inner" />
-			</span>
-
-			<span className="checkbox__label">{props.children}</span>
-		</label>
-	);
+interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement> {
+	label?: string;
 }
+
+const CheckBox = forwardRef(
+	(props: CheckBoxProps, ref: Ref<HTMLInputElement>) => {
+		return (
+			<label
+				className={`checkbox ${
+					props.disabled ? "checkbox--disabled" : ""
+				} ${props.className}`}
+			>
+				<span className="checkbox__toggle">
+					<input
+						ref={ref}
+						type="checkbox"
+						className="checkbox__input"
+						aria-hidden
+						onChange={props.onChange}
+					/>
+					<span className="checkbox__toggle__inner" />
+				</span>
+
+				<span className="checkbox__label">{props.children}</span>
+			</label>
+		);
+	}
+);
+export default CheckBox;
