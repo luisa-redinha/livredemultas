abb6e3d3cfdcec6ad9bc31369a97be6ed4fd8368
Rafa Lopes <rafalopessecond@gmail.com>
2024-09-25 22:34:51 +0100

    combobox update

--- components/input/ComboBox.tsx
+++ components/input/ComboBox.tsx
@@ -1,18 +1,31 @@
 "use client";
 
+import { KeyboardEvent, KeyboardEventHandler } from "react";
 import Select from "react-select";
 
 export type ComboBoxOption = { label: string; value: string | number };
 
 export type ComboBoxProps = {
 	data: ComboBoxOption[];
+	onSelect?: Function;
+	onKeyDown?: Function;
 };
 
-export default function ComboBox({ data }: ComboBoxProps) {
+export default function ComboBox({ data, onSelect, onKeyDown }: ComboBoxProps) {
+	const onKeyDownHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+		const validKey = /^[\w\d -_]{1}$/gi;
+		if (validKey.test(e.key)) {
+			onKeyDown && onKeyDown((e.target as HTMLInputElement).value);
+		}
+	};
+
 	return (
 		<Select
 			options={data}
+			onChange={(n, a) => onSelect && onSelect(n?.label)}
+			onKeyDown={onKeyDownHandler}
 			className="input combobox"
+			isSearchable
 			isClearable
 			placeholder="Pesquisar..."
 		/>
