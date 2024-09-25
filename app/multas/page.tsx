da30d036b6d562da68069f5f683aa04f8e9130c6
Rafa Lopes <rafalopessecond@gmail.com>
2024-09-25 22:39:57 +0100

    add back button

--- app/multas/page.tsx
+++ app/multas/page.tsx
@@ -5,6 +5,8 @@ import ComboBox from "@/components/input/ComboBox";
 import React, { useState } from "react";
 import multas from "./multas.json";
 import SectionThumbnail from "@/components/buttons/SectionThumbnail";
+import Button from "@/components/buttons/Button";
+import Link from "next/link";
 export default function page() {
 	const comboBoxData = multas.map((e, i) => ({
 		label: e.label,
@@ -34,13 +36,22 @@ export default function page() {
 
 	return (
 		<Section className="multas">
-			<h2>Tipos de Multas</h2>
-			<p>
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
-				placeat quia mollitia nesciunt, molestias, impedit eveniet vero
-				maiores nostrum numquam excepturi debitis consectetur fuga amet
-				vitae velit, laborum deserunt sunt?
-			</p>
+			<header>
+				<h2>Tipos de Multas</h2>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					Illo placeat quia mollitia nesciunt, molestias, impedit
+					eveniet vero maiores nostrum numquam excepturi debitis
+					consectetur fuga amet vitae velit, laborum deserunt sunt?
+				</p>
+
+				<Link
+					className="button button--secondary"
+					href="/"
+				>
+					Início
+				</Link>
+			</header>
 
 			<div className="search">
 				<h3>Pesquisar</h3>
