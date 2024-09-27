02aa43a51982e744810d9c1eaf56e5fbf42cef25
Rafa Lopes <rafalopessecond@gmail.com>
2024-09-27 11:51:01 +0100

    update faq

--- app/sections/FAQ.tsx
+++ app/sections/FAQ.tsx
@@ -3,22 +3,24 @@ import Section from "@/components/containers/Section";
 import faqData from "./faq.json";
 export default function FAQ() {
 	return (
-		<Section className="faq">
+		<Section>
 			<h2>Perguntas Frequentes</h2>
-			<Accordion>
-				{faqData.map((e, i) => (
-					<AccordionItem
-						header={e.q}
-						key={`faq-q${i}`}
-					>
-						<ul>
-							{e.a.map((p, j) => (
-								<li key={`faq-q${i}-ap-${j}`}>{p}</li>
-							))}
-						</ul>
-					</AccordionItem>
-				))}
-			</Accordion>
+			<div className="faq">
+				<Accordion>
+					{faqData.map((e, i) => (
+						<AccordionItem
+							header={e.q}
+							key={`faq-q${i}`}
+						>
+							<ul>
+								{e.a.map((p, j) => (
+									<li key={`faq-q${i}-ap-${j}`}>{p}</li>
+								))}
+							</ul>
+						</AccordionItem>
+					))}
+				</Accordion>
+			</div>
 		</Section>
 	);
 }
