65a325d1e19990cc739fdb6eed520d22327b9fb9
Rafa Lopes <rafalopessecond@gmail.com>
2024-10-18 18:16:52 +0100

    fuck it push it all

--- app/api/sendMail/route.ts
+++
@@ -1,7 +0,0 @@
-// import type { NextApiRequest, NextApiResponse } from "next";
-
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-// 	// const  {content,attachments} = req.body;
-
-// 	res.status(200).json("Ok");
-// }
