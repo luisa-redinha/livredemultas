a580e806ce4ed68e552e30adbeb191ad5d9c1f8c
Rafa Lopes <rafalopessecond@gmail.com>
2024-10-27 23:37:34 +0000

    add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./main.css";
 import { AnimatePresence } from "framer-motion";
 import { Metadata } from "next";
 import Contacts from "./sections/Contacts";
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: "Livre de Multas",
@@ -19,16 +20,23 @@ export default function RootLayout({
 	return (
 		<html lang="pt">
 			<head>
-				{" "}
 				<link
 					rel="icon"
 					href="/favicon.png"
 				/>
-				<script
+				<Script
+					id="font-awesome"
 					src="https://kit.fontawesome.com/1a62655504.js"
 					crossOrigin="anonymous"
 					async
-				></script>
+				></Script>
+				<Script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=AW-16743308519"
+				></Script>
+
+				<Script src="/gtag.js" />
+
 			</head>
 			<body>
 				<div id="bg" />
