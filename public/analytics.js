4704002f30371b41861f5a1ff0bed719d8f3f726
Rafa Lopes <rafalopessecond@gmail.com>
2024-12-02 11:48:17 +0000

    analytics snippet update

--- public/analytics.js
+++ public/analytics.js
@@ -6,33 +6,31 @@ gtag("js", new Date());
 
 gtag("config", "G-RVTPNK8QHL");
 
-// <!-- Event snippet for WhatsApp conversion page -->
-
 window.addEventListener("DOMContentLoaded", function () {
-	document.addEventListener("click", function (e) {
-		if (e.target.closest('a[href*="wa.me"]')) {
+	document.body.addEventListener("click", function (e) {
+		const link = e.target.closest('a[href*="wa.me"]');
+		if (link) {
 			gtag("event", "conversion", {
 				send_to: "AW-16743308519/vLSlCMDG1_EZEOe56q8-",
 			});
 		}
 	});
-});
-// <!-- Event snippet for Formulário conversion page -->
 
-document.addEventListener("DOMContentLoaded", function () {
-	const form = document.querySelector("#meuFormulario");
+	const form = document.querySelector(".form");
+	if (form) {
+		form.addEventListener("submit", function (e) {
+			e.preventDefault();
 
-	form.addEventListener("submit", function (e) {
-		e.preventDefault();
+			if (e.target.checkValidity()) {
+				window.dataLayer = window.dataLayer || [];
+				window.dataLayer.push({ event: "form_enviado" });
 
-		if (e.target.checkValidity()) {
-			window.dataLayer = window.dataLayer || [];
-			window.dataLayer.push({ event: "form_enviado" });
-			gtag("event", "conversion", {
-				send_to: "AW-16743308519/wirGCL3G1_EZEOe56q8-",
-			});
+				gtag("event", "conversion", {
+					send_to: "AW-16743308519/wirGCL3G1_EZEOe56q8-",
+				});
 
-			e.target.submit();
-		}
-	});
+				e.target.submit();
+			}
+		});
+	}
 });
