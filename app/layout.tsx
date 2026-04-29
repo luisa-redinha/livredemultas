59b826f559e558131d391a54e815a04eb020152e
LuisaRedinha <webmaster@livredemultas.pt>
2026-04-29 10:10:38 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,25 +41,44 @@ export default function RootLayout({
 					crossOrigin="anonymous"
 					async
 				></Script>
-				{/* GTAG */}
-				<Script
-					async
-					src="https://www.googletagmanager.com/gtag/js?id=AW-17992909738"
-				></Script>
-				<Script
-					async
-					src="/gtag.js"
-				/>
+				// 1. Importa o componente (se usares a biblioteca oficial do Next)
+// Ou usa o componente Script do Next.js como já estavas a fazer, mas com o código correto:
 
-				{/* Analytics */}
-				<Script
-					async
-					src="https://www.googletagmanager.com/gtag/js?id=GT-K4CTR5TF"
-				></Script>
-				<Script
-					async
-					src="/analytics.js"
-				></Script>
+import Script from "next/script";
+
+// ... dentro do RootLayout, antes do </head>
+<head>
+  {/* Google Tag Manager */}
+  <Script
+    id="gtm-script"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-XXXXXXX'); // SUBSTITUI PELO TEU ID GTM
+      `,
+    }}
+  />
+</head>
+
+// ... logo após o <body>
+<body>
+  {/* Google Tag Manager (noscript) */}
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX" // SUBSTITUI PELO TEU ID GTM
+      height="0"
+      width="0"
+      style={{ display: 'none', visibility: 'hidden' }}
+    />
+  </noscript>
+  {/* Resto dos teus providers */}
+  <ToastProvider />
+  ...
+</body>
 			</head>
 			<body>
 				<ToastProvider />
